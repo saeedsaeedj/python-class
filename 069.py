d7c44a7ba55b4efe560b78ff1e93763ea06f446d
saeedsaeedj <52496027+saeedsaeedj@users.noreply.github.com>
2020-07-29 12:20:43 +0430

    Update 069.py

--- 069.py
+++ 069.py
@@ -1,5 +1,6 @@
-count=('iran','turky','araq','swiss','azarbayjan')
-print (count)
-s=input('input counter of tuple : ')
-if (s==count.index(s)):
-    print (count.index(s))
+country=('iran','turky','araq','swiss','azarbayjan')
+print (country)
+s=input('input counter of tuple :')
+if s in country:
+    print(s)
+    print(country.index(s))
